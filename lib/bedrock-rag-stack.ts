83b8d9024ef9ab420d2bce8e23d9dc36aae1f43d
unknown <touqeer_ahson@hotmail.com>
2026-01-01 16:05:11 +1100

    db deployed and tested with local connection using bastion.

--- lib/bedrock-rag-stack.ts
+++ lib/bedrock-rag-stack.ts
@@ -7,7 +7,7 @@ export class BedrockRagCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // VPC (cheap: no NAT gateways)
+    // VPC Creation
     const vpc = new ec2.Vpc(this, "RagVpc", {
       maxAzs: 2,
       natGateways: 0,
@@ -20,7 +20,7 @@ export class BedrockRagCdkStack extends cdk.Stack {
       description: "Security group for Postgres",
     });
 
-    // Postgres RDS
+    // Postgres RDS,
     const db = new rds.DatabaseInstance(this, "RagPostgres", {
       engine: rds.DatabaseInstanceEngine.postgres({
         version: rds.PostgresEngineVersion.VER_15,
@@ -37,7 +37,7 @@ export class BedrockRagCdkStack extends cdk.Stack {
       allocatedStorage: 20,
       maxAllocatedStorage: 30,
       securityGroups: [dbSg],
-      publiclyAccessible: false,
+      publiclyAccessible: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       deletionProtection: false,
     });
@@ -45,5 +45,38 @@ export class BedrockRagCdkStack extends cdk.Stack {
     new cdk.CfnOutput(this, "DbEndpoint", {
       value: db.instanceEndpoint.hostname,
     });
+
+    const bastionSg = new ec2.SecurityGroup(this, "BastionSg", {
+      vpc,
+      description: "Security group for bastion host",
+      allowAllOutbound: true,
+    });
+
+    // Allow SSH only from your IP
+    bastionSg.addIngressRule(
+      ec2.Peer.ipv4("115.129.76.74/32"),
+      ec2.Port.tcp(22),
+      "SSH access from my IP"
+    );
+
+    const bastion = new ec2.Instance(this, "BastionHost", {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+      securityGroup: bastionSg,
+      keyName: "bastion-key", // must exist in EC2 Key Pairs
+    });
+
+    db.connections.allowFrom(
+      bastion,
+      ec2.Port.tcp(5432),
+      "Bastion access to Postgres"
+    );
   }
 }
