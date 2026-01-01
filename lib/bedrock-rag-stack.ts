0009d295f6b3b005d16cb321ddba381db63814bd
unknown <touqeer_ahson@hotmail.com>
2026-01-01 11:13:13 +1100

    initial cdk, cdk synth is working. have not deplyed yet

--- lib/bedrock-rag-stack.ts
+++ lib/bedrock-rag-stack.ts
@@ -1,16 +1,49 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
 
-export class BedrockRagStack extends cdk.Stack {
+export class BedrockRagCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC (cheap: no NAT gateways)
+    const vpc = new ec2.Vpc(this, "RagVpc", {
+      maxAzs: 2,
+      natGateways: 0,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BedrockRagQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Security Group for DB
+    const dbSg = new ec2.SecurityGroup(this, "DbSecurityGroup", {
+      vpc,
+      allowAllOutbound: true,
+      description: "Security group for Postgres",
+    });
+
+    // Postgres RDS
+    const db = new rds.DatabaseInstance(this, "RagPostgres", {
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_15,
+      }),
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO
+      ),
+      credentials: rds.Credentials.fromGeneratedSecret("raguser"),
+      allocatedStorage: 20,
+      maxAllocatedStorage: 30,
+      securityGroups: [dbSg],
+      publiclyAccessible: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+    });
+
+    new cdk.CfnOutput(this, "DbEndpoint", {
+      value: db.instanceEndpoint.hostname,
+    });
   }
 }
